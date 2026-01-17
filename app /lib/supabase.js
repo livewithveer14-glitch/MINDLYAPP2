f866bd3c9982de909c152bdd1de055d69e1cbe16
livewithveer14-glitch <livewithveer14@gmail.com>
2026-01-17 11:08:49 -0800

    Delete app /lib directory

--- app /lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://uzbqazweaggpitjgdrbg.supabase.co'
-const supabaseKey = 'sb_publishable_WT79EyrZ09pUdgtsJPZ86g_lHf_Wug5'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
