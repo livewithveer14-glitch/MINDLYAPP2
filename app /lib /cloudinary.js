e6ee09702d1ee7a773417917b6083394babf7d36
livewithveer14-glitch <livewithveer14@gmail.com>
2026-01-17 11:08:02 -0800

    Create cloudinary.js

---
+++ app /lib /cloudinary.js
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from 'cloudinary'
+
+cloudinary.config({
+  cloud_name: 'dld9gyvbd',
+  api_key: '881179828571332',
+  api_secret: 'ah55iGYWygUBawYvmEzmr0VDiqQ'
+})
+
+export default cloudinary
