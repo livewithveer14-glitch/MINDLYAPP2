8632d11a2cacf60b514b0c5aacdb5f1ab4c58c57
livewithveer14-glitch <livewithveer14@gmail.com>
2026-01-17 20:51:51 -0800

    Delete app /lib  directory

--- app /lib /cloudinary.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from 'cloudinary'
-
-cloudinary.config({
-  cloud_name: 'dld9gyvbd',
-  api_key: '881179828571332',
-  api_secret: 'ah55iGYWygUBawYvmEzmr0VDiqQ'
-})
-
-export default cloudinary
